8d7c8ae79ed4473a08ca149c4ea04aa958c86ce3
Brendt Bly <brendt.bly@outlook.com>
2020-05-08 09:09:32 -0500

    Initial commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Home } from '../components/home.component';
+import { HomeLayout } from '../layouts/home.layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeLayout,
+    children: [
+      {
+        path: '', component: Home,
+      },
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+      declarations: [
+        Home,
+      ],
+      imports: [RouterModule.forRoot(routes)],
+      exports: [RouterModule]
+    })
 export class AppRoutingModule { }
