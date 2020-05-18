cf581b22584557b0eb3c2db531f929e5e767a41c
Brendt Bly <brendt.bly@outlook.com>
2020-05-18 11:12:58 -0500

    Added video id to route params and added video to template.

--- client/src/components/play-video/play-video.component.ts
+++ client/src/components/play-video/play-video.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { createVideoScript } from 'src/util/util';
 
 @Component({
   selector: 'app-play-video',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play-video.component.scss']
 })
 export class PlayVideoComponent implements OnInit {
+  videoId;
+  height: string;
+  width: string;
 
-  constructor() { }
+  constructor(  private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    //TODO: may need to check to make sure script tag for youtube video has been added.
+    createVideoScript();
+
+    this.initializeVariables();
   }
 
+  initializeVariables() {
+    this.route.parent.params.subscribe(params => { 
+      this.videoId = params.id;
+    });
+    console.log(this.videoId);
+  }
 }
